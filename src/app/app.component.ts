1b74b178c78b8e603c47af4e582a9b7d3fb3cead
Andrea Gatta <andrew@gat.tax>
2025-03-30 00:36:13 +0100

    prova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {RouterOutlet} from '@angular/router';
 import {ThemeService} from './core/services/theme.service';
 import {LoginComponent} from './components/login/login.component';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,8 +21,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.themeService.activeTheme$.subscribe((theme: string) => {
       document.body.className = theme; // Imposta la classe nel body
+      console.log("sesso");
     });
   }
+
 
   toggleTheme() {
     const newTheme = this.themeService.getTheme() === 'light' ? 'dark' : 'light';
